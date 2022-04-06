f8e9d03d181e622d2c9706df3c33f5641945810f
Lance Hebert <LSUHEBERT@gmail.com>
2022-04-05 19:41:33 -0700

    Added notes and practice problems

---
+++ notes.js
@@ -0,0 +1,16 @@
+// Space complexity = 0(1) for most primitives
+// O(n) for reference types like array and obj, and strings
+
+
+// Logarithms
+// Assumed to be Logsub2 = exponent
+//  so to figure out Log2(8)=3 === 2^3 = 8
+
+
+// when adding new data to an array its normally O(1) time unless adding to the front of the array becuase it has to go through and change every index. So now it has to go through O(N) things. Same thing with getting rid of something at the front of an array.
+
+// push and pop always faster than shift and unshift
+
+// shift and unshift are named that because they have to redo the whole index after it. Mindblown.
+
+// 
